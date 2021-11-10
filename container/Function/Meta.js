7a0e8b12507869f04980942ad87083564d64de49
poom-sci <poom.suchaoin@gmail.com>
2021-11-10 11:17:26 +0700

    fix(metatag): change website url

--- container/Function/Meta.js
+++ container/Function/Meta.js
@@ -11,19 +11,19 @@ export default function MetaTag(props) {
       <meta httpEquiv="content-language" content="th" />
       <meta httpEquiv="content-type" content="text/html" charSet="UTF-8" />
       <meta name="revisit-after" content="2 days" />
-      <link rel="canonical" href={'https://poomsci.web.app/' + props.path} />
+      <link rel="canonical" href={'https://poom-sci.vercel.app/' + props.path} />
 
       <meta property="og:title" content={props.title}></meta>
       <meta property="og:site_name" content="Poom-sci"></meta>
       <meta property="og:type" content="website"></meta>
-      <meta property="og:image" content="https://poomsci.web.app/cover/poom-web-cover.jpg" />
+      <meta property="og:image" content="https://poom-sci.vercel.app/cover/poom-web-cover.jpg" />
       <meta property="og:description" content={props.description} />
-      <meta property="og:url" content="https://poomsci.web.app/" />
+      <meta property="og:url" content="https://poom-sci.vercel.app/" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={props.title} />
       <meta name="twitter:description" content={props.description} />
-      <meta name="twitter:image" content="https://poomsci.web.app/cover/poom-web-cover.jpg"></meta>
+      <meta name="twitter:image" content="https://poom-sci.vercel.app/cover/poom-web-cover.jpg"></meta>
       <meta name="twitter:site" content="Poom-sci"></meta>
     </>
   );
