0ed8064ff99198119e5726a2287463a70b5a91dc
poom-sci <poom.suchaoin@gmail.com>
2021-11-15 18:48:05 +0700

    fix tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import dynamic from 'next/dynamic';
 
-const Tag = dynamic(() => import('../container/Function/TagHeader'));
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -13,7 +12,18 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} /> */}
-          <Tag />
+          <meta name="google-site-verification" content="CFJplFjLKydgWqcLRCBf6u9z_bHko_lV3p_aQ0v9l-A" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <meta name="google-site-verification" content="kqg5HW-tLoFQit1C0JqdlMAzxCYtpbThKhO0wgnHwtQ" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;600;700;800;900&family=Merriweather:wght@300;400;700&family=Quicksand:wght@300;400;500;600;700&family=Mitr:wght@200;300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          ></link>
           {/*
               font-family: 'Alegreya', serif;
               font-family: 'Merriweather', serif;
